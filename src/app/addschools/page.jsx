0bb3b235277f1b6a1fd7da796063f8935e7a23c5
Will <rrozario958@gmail.com>
2024-07-24 13:41:40 +0530

    complete frontend

---
+++ src/app/addschools/page.jsx
@@ -0,0 +1,153 @@
+"use client";
+
+import React from "react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import styles from "./page.module.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useRouter } from "next/navigation";
+import { Link } from "next/link";
+
+export const imgURLbuilder = (image) => {
+  return "http://localhost:5000/" + image;
+};
+
+const AddSchools = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting, isSubmitSuccessful },
+  } = useForm();
+
+  const router = useRouter();
+
+  const onSubmit = async (data) => {
+    const formData = new FormData();
+    formData.append("name", data.name);
+    formData.append("email", data.email);
+    formData.append("address", data.address);
+    formData.append("state", data.state);
+    formData.append("city", data.city);
+    formData.append("contact", data.contact);
+    formData.append("image", data.image[0]);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/schools",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      console.log("School added successfully:", response.data);
+
+      reset();
+
+      router.push("/");
+      toast.success("School added successfully");
+    } catch (error) {
+      console.error("Error adding school:", error);
+
+      toast.error("Error adding school");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+      <h1 className={styles.title}>Add School</h1>
+      <div>
+        <label className={styles.label}>School Name:</label>
+        <input
+          type="text"
+          {...register("name", { required: "School name is required" })}
+          className={styles.inputText}
+        />
+        {errors.name && <p className={styles.error}>{errors.name.message}</p>}
+      </div>
+
+      <div>
+        <label className={styles.label}>Email:</label>
+        <input
+          type="email"
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: "Invalid email address",
+            },
+          })}
+          className={styles.inputEmail}
+        />
+
+        {errors.email && <p className={styles.error}>{errors.email.message}</p>}
+      </div>
+      <div>
+        <label className={styles.label}>Address:</label>
+        <input
+          type="text"
+          {...register("address", { required: "Address is required" })}
+          className={styles.inputText}
+        />
+        {errors.address && (
+          <p className={styles.error}>{errors.address.message}</p>
+        )}
+      </div>
+      <div>
+        <label className={styles.label}>State:</label>
+        <input
+          type="text"
+          {...register("state", { required: "State is required" })}
+          className={styles.inputText}
+        />
+        {errors.state && <p className={styles.error}>{errors.state.message}</p>}
+      </div>
+
+      <div>
+        <label className={styles.label}>City:</label>
+        <input
+          type="text"
+          {...register("city", { required: "City is required" })}
+          className={styles.inputText}
+        />
+        {errors.city && <p className={styles.error}>{errors.city.message}</p>}
+      </div>
+
+      <div>
+        <label className={styles.label}>Contact no</label>
+        <input
+          type="number"
+          {...register("contact", { required: "Contact no is required" })}
+          className={styles.inputText}
+        />
+        {errors.contact && (
+          <p className={styles.error}>{errors.contact.message}</p>
+        )}
+      </div>
+      <div>
+        <label className={styles.label}>Image:</label>
+        <input
+          type="file"
+          {...register("image", { required: "Image is required", maxFiles: 1 })}
+          className={styles.inputFile}
+        />
+
+        {errors.image && <p className={styles.error}>{errors.image.message}</p>}
+      </div>
+      <div className="button-group">
+        <button type="submit" className={styles.button}>
+          {isSubmitting ? "Submitting..." : "Submit"}
+        </button>
+        <button className={styles.button} onClick={() => router.back()}>
+          Back
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default AddSchools;
--- src/app/page.js
+++ src/app/page.js
@@ -1,94 +1,68 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { FidgetSpinner } from "react-loader-spinner";
 
 export default function Home() {
+  const [schools, setSchools] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchSchools = async () => {
+      const response = await fetch("http://localhost:5000/api/schools");
+      const data = await response.json();
+      setSchools(data);
+      setLoading(false);
+    };
+
+    fetchSchools();
+  }, []);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main>
+      <h1 className={styles.title}>Schools List</h1>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
+      <button
+        className={styles.button}
+        onClick={() => router.push("/addschools")}
+      >
+        {" "}
+        Add School
+      </button>
       <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
+        {loading ? (
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            <FidgetSpinner
+              visible={true}
+              height="80"
+              width="80"
+              ariaLabel="fidget-spinner-loading"
+              wrapperStyle={{}}
+              wrapperClass="fidget-spinner-wrapper"
+            />
           </p>
-        </a>
+        ) : schools.message === "No schools created yet" ? (
+          <p>No schools found</p>
+        ) : (
+          schools?.data?.map((school) => (
+            <div key={school.id} className={styles.card}>
+              <Image
+                src={`http://localhost:5000/${school.image}`}
+                alt="School image"
+                width={200}
+                height={200}
+              />
+              <h2 className={styles.name}>{school.name}</h2>
+              <p className={styles.city}>{school.city}</p>
+              <p className={styles.address}>{school.address}</p>
+            </div>
+          ))
+        )}
       </div>
     </main>
   );
